0fcc531971b7af6ffaae2a894d2f75af58940533
sadwan <sadwan@CO-IT-105>
2025-10-19 15:10:20 +0600

    Changes to be committed: 	modified:   src/modules/form-builder/form-preview.vue

--- src/modules/form-builder/form-preview.vue
+++ src/modules/form-builder/form-preview.vue
@@ -1,5 +1,5 @@
 <template>
-    <VCard title="Form Preview" style="background-color: rgb(245, 245, 245);">
+    <VCard title="Supplier Profile Management" style="background-color: rgb(245, 245, 245);">
           <VCardText>
             <VTabs
                 v-model="activeTab"
